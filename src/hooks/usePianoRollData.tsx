d73bc11362123d4d28088b86542f636d7bafac8c
GeniaPiano <genia.piano@hotmail.com>
2023-10-31 21:16:08 +0100

    Added Navbar and usePianoRollsData hook

---
+++ src/hooks/usePianoRollData.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+const usePianoRollData = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch('https://pianoroll.ai/random_notes');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                setData(data);
+            } catch (error) {
+                console.error('Error loading data:', error);
+            }
+        })();
+    }, []);
+
+    return data;
+};
+
+export default usePianoRollData;
