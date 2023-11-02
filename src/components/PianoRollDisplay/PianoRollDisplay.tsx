67e6e227a0ed6a9a52d074858502c88330448d8f
GeniaPiano <genia.piano@hotmail.com>
2023-11-02 18:57:14 +0100

    Updated PianoRollCard and PianoDisplay

--- src/components/PianoRollDisplay/PianoRollDisplay.tsx
+++ src/components/PianoRollDisplay/PianoRollDisplay.tsx
@@ -1,9 +1,12 @@
 import './PianoRollDisplay.css'
 import {usePianoRollData} from "../../hooks/usePianoRollData";
 import {PianoRollCard} from "../PianoRollCard/PianoRollCard";
+import {useSelectedRollAndView} from "../../providers/SelectedRollAndViewProvider";
 
-export const PianoRollDisplay = () => {
+
+export const PianoRollDisplay = ({setShowPianoRoll}) => {
     const { data, loading } = usePianoRollData();
+    const {selectedPianoRoll} = useSelectedRollAndView()
 
     if (loading) {
         return <div>Loading...</div>;
@@ -14,11 +17,16 @@ export const PianoRollDisplay = () => {
     }
 
     return (
-        <div className="pianoRollContainer">
-            {data.slice(0, 20).map((rollData, index) =>
-                <PianoRollCard key={index} index={index} data={rollData} />)
-            }
-        </div>
+            <div className="grid-container" >
+                {data.slice(0, 20).map((rollData, index) =>
+                    <PianoRollCard
+                        key={index}
+                        index={index}
+                        data={rollData}
+                        setShowPianoRoll={setShowPianoRoll}
+                        isSelected/>)
+                }
+            </div>
     )
 }
 
