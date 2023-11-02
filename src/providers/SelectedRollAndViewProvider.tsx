4724949a3bd70676e58b8ed5c5ba7590d101a921
GeniaPiano <genia.piano@hotmail.com>
2023-11-02 18:49:51 +0100

    Added SelectedRollAndProvider

---
+++ src/providers/SelectedRollAndViewProvider.tsx
@@ -0,0 +1,49 @@
+import {createContext, useContext, useState} from "react";
+
+interface SelectedRollContext {
+    isGridView: boolean;
+    isMainView: boolean;
+    selectedPianoRoll: number | null;
+    handleSelectRoll: (id: number)=> void;
+    changeGridView: (bool: boolean) => void;
+    changeIsMainView: (bool: boolean) => void;
+}
+
+const SelectedRollContext = createContext<SelectedRollContext | undefined>(undefined);
+
+export const SelectedRollProvider = ({ children }) => {
+    const [isGridView, setIsGridView] = useState(false);
+    const [selectedPianoRoll, setSelectedPianoRoll] = useState<number | null>(null);
+    const [isMainView, setIsMainView] = useState(false);
+
+    const changeGridView =(bool: boolean)=> setIsGridView(bool);
+    const changeIsMainView =(bool: boolean)=> setIsMainView(bool);
+
+    const handleSelectRoll = (id) => {
+        setSelectedPianoRoll(id)
+    }
+
+    return (
+        <SelectedRollContext.Provider value={{
+            isGridView,
+            isMainView,
+            handleSelectRoll,
+            changeGridView,
+            selectedPianoRoll,
+            changeIsMainView,
+
+        }}>
+            {children}
+        </SelectedRollContext.Provider>
+    );
+};
+
+export const useSelectedRollAndView = () => {
+    const context = useContext(SelectedRollContext);
+
+    if (!context) {
+        throw new Error('useSelectedRoll must be used within a SelectedRollProvider');
+    }
+
+    return context;
+};
